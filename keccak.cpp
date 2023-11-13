450209395a80a777be0e54894677f9f294231d1d
VolodymyrNakonechnyi <146861887+VolodymyrNakonechnyi@users.noreply.github.com>
2023-11-13 13:43:23 +0200

    Update keccak.cpp

--- keccak.cpp
+++ keccak.cpp
@@ -3,12 +3,18 @@
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define ROL64(a, offset) ((((uint64_t)a) << offset) ^ (((uint64_t)a) >> (64-offset)))
 
-const uint64_t roundConstants[24] = { 0x0000000000000001, 0x0000000000008082, 0x800000000000808A, 0x8000000080008000,
-0x000000000000808B, 0x0000000080000001, 0x8000000080008081, 0x8000000000008009,
-0x000000000000008A, 0x0000000000000088, 0x0000000080008009, 0x000000008000000A,
-0x000000008000808B, 0x800000000000008B, 0x8000000000008089, 0x8000000000008003,
-0x8000000000008002, 0x8000000000000080, 0x000000000000800A, 0x800000008000000A,
-0x8000000080008081, 0x8000000000008080, 0x0000000080000001, 0x8000000080008008 };
+
+const uint64_t RC[24] =
+{
+  0x0000000000000001, 0x0000000000008082, 0x800000000000808a,
+  0x8000000080008000, 0x000000000000808b, 0x0000000080000001,
+  0x8000000080008081, 0x8000000000008009, 0x000000000000008a,
+  0x0000000000000088, 0x0000000080008009, 0x000000008000000a,
+  0x000000008000808b, 0x800000000000008b, 0x8000000000008089,
+  0x8000000000008003, 0x8000000000008002, 0x8000000000000080,
+  0x000000000000800a, 0x800000008000000a, 0x8000000080008081,
+  0x8000000000008080, 0x0000000080000001, 0x8000000080008008
+};
 
 uint64_t load64(const uint8_t* x)
 {
@@ -39,8 +45,6 @@ void store64(uint8_t* x, uint64_t u, int index)
     }
 }
 
-//===========================================================//
-
 uint64_t laneRead(int x, int y, uint8_t* state) {
     uint8_t result[8] = { 0 };
 
@@ -62,7 +66,8 @@ void laneXOR(int x, int y, uint8_t* state, uint64_t xor_value) {
 
 
 
-void thetaStep(uint8_t* state) {
+void thetaStep(uint8_t* state) 
+{
     uint64_t C[5], D;
 
     for (int x = 0; x < 5; x++) {
@@ -80,52 +85,55 @@ void thetaStep(uint8_t* state) {
     }
 }
 
-void rhoStep(uint8_t* state) {
-    int x = 1, y = 0;
+void rhoAndPiStep(uint8_t* state) 
+{
+    uint64_t current, temp;
 
-    for (int t = 0; t < 23; t++) {
-        for (int z = 0; z < 8; z++) {
-            state[(x * 8) + (y * 40) + z] = state[(x * 8) + (y * 40) + ((z - (t + 1) * (t + 2) / 2) % 64)];
-        }
+    uint32_t x = 1, y = 0;
+    current = laneRead(x, y, state);
+    for (int t = 0; t < 24; t++) {
+        uint32_t r = ((t + 1) * (t + 2) / 2) % 64;
+        uint32_t Y = (2 * x + 3 * y) % 5; x = y; y = Y;
 
-        int temp = x;
-        x = y;
-        y = (2 * x + 3 * y) % 5;
+        temp = laneRead(x, y, state);
+        laneWrite(x, y, state, ROL64(current, r));
+        current = temp;
     }
 }
 
-void piStep(uint8_t* state) {
-    for (int x = 0; x < 5; x++) {
-        for (int y = 0; y < 5; y++) {
-            laneWrite(x, y, state, laneRead((x + 3 * y) % 5, x, state));
-        }
-    }
-}
+void chiStep(uint8_t* state) 
+{
+    uint64_t temp[5];
 
-void chiStep(uint8_t* state) {
-    for (int x = 0; x < 5; x++) {
-        for (int y = 0; y < 5; y++) {
-            laneWrite(x, y, state, (laneRead(x, y, state) ^ ( (laneRead((x + 1) % 5, y, state) ^ 1) * (laneRead((x + 2) % 5, y, state) ) )));
+    for (int y = 0; y < 5; y++)
+    {
+        for (int x = 0; x < 5; x++)
+        {
+            temp[x] = laneRead(x, y, state);
+        }
+
+        for (int x = 0; x < 5; x++)
+        {
+            laneWrite(x, y, state, temp[x] ^ ((~temp[(x + 1) % 5]) & (temp[(x + 2) % 5])) );
         }
     }
 }
 
 void iotaStep(uint8_t* state, int round) {
-    laneXOR(0, 0, state, roundConstants[round]);
+    laneXOR(0, 0, state, RC[round % 24]);
 }
 
 void keccak_permutation_1600(uint8_t*state) {
     for (uint32_t round = 0; round < 24; round++) {
         thetaStep(state);
-        rhoStep(state);
-        piStep(state);
+        rhoAndPiStep(state);
         chiStep(state);
         iotaStep(state, round);
     }
 }
 
 
-void keccak(int rate, int capacity, uint8_t input[], int input_length, uint8_t delimitedSuffix, uint8_t * output,int output_length) {
+void keccak(int rate, int capacity,const char* input, int input_length, uint8_t delimitedSuffix, uint8_t * output,int output_length) {
     uint8_t state[200] = { 0 };
     unsigned int rateInBytes = rate / 8;
     unsigned int blockSize = 0;
@@ -172,7 +180,7 @@ int main()
 
     uint8_t input[3] = { 97, 98, 99 };
 
-    keccak(1088, 512, input, 3, 0x06, output, 64);
+    keccak(1088, 512, input_string, 3, 0x06, output, 64);
 
     std::cout << ("SHA-3 256-bit Hash:");
     for (int i = 0; i < 32; i++) {
